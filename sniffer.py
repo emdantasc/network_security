2ceeeab97ce05dd50ec3f273ff807ee2190644e6
emdantasc <emdantasc@ufrn.edu.br>
2018-10-12 13:29:30 -0300

    code done

--- sniffer.py
+++ sniffer.py
@@ -1,10 +1,9 @@
 from scapy.all import *
 
 def read_packet(packet):
-    if packet[TCP].payload and packet[TCP].dport==80:
-            print("\nHTTP Capture: \nRequest from: "+packet[IP].src+":"+str(packet[IP].dport)
-                +"\nServer at: "+packet[IP].dst+":"+str(packet[IP].dport)
-                +"\nContent: "+str(bytes(packet[TCP].payload)))
-                
+    if packet[TCP].payload:
+            print("\nHTTP Capture: \nRequest from: "+packet[IP].src+":"+str(packet[IP].sport)
+                +"\nServer at: "+packet[IP].dst+":"+str(packet[IP].dport))
+            
 
-sniff(iface='enp7s0', filter='tcp', prn=read_packet, store=0)
+sniff(iface='enp7s0', filter='tcp port 80', prn=read_packet, store=0)
